f102217832501f22064fd80c12a938afcb7d3abf
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-09-03 18:51:56 +0100

    removing flex-col

--- src/pages/Home/Collections.jsx
+++ src/pages/Home/Collections.jsx
@@ -2,11 +2,12 @@
 function Collections() {
     return (
         <div className="bg-[url('/src/assets/images/zara.jpg')] bg-cover bg-center bg-no-repeat xl:px-28 px-4 my-20">
-            <div className="h-[580px] flex flex-col justify-between md:flex-row items-center">
+            <div className="h-[560px] flex justify-between md:flex-row items-center">
                 <div className="md:w-1/2"></div>
                 <div className="md:w-1/2">
                     {/* an image should be here */}
-                    <p>Lustrous yet understated. The new evening wear collection exclusively offered at the reopened Giorgio Amani boutique in Los Angeles</p>
+                    <p className="text-lg text-blue-700 capitalize font-bold my-8 leading-[32px] md:w-2/3">Lustrous yet understated. The new evening wear collection exclusively offered at the reopened Giorgio Amani boutique in Los Angeles</p>
+                    <button className="px-6 py-2 bg-white text-black rounded-sm font-semibold">See Collections</button>
                 </div>
             </div>
         </div>
