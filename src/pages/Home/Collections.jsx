3f1a150bd6edc6862c69709c7158f1697011369a
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-09-06 11:10:24 +0100

    collections

--- src/pages/Home/Collections.jsx
+++ src/pages/Home/Collections.jsx
@@ -1,14 +1,14 @@
 
 function Collections() {
     return (
-        <div className="bg-[url('../../../public/images/zara.jpg')] bg-cover bg-center bg-no-repeat xl:px-28 px-4 my-20">
-            <div className="h-[560px] flex justify-between md:flex-row items-center">
-                <div className="md:w-1/2"></div>
+        <div className="bg-[url('../../../public/images/banner.jpg')] bg-cover bg-center bg-no-repeat xl:px-28 px-4 my-20">
+            <div className="h-[560px] flex justify-between md:flex-row items-center">    
                 <div className="md:w-1/2">
                     {/* an image should be here */}
-                    <p className="text-lg text-blue-700 capitalize font-bold my-8 leading-[32px] md:w-2/3">Lustrous yet understated. The new evening wear collection exclusively offered at the reopened Giorgio Amani boutique in Los Angeles</p>
-                    <button className="px-6 py-2 bg-white text-black rounded-sm font-semibold">See Collections</button>
+                    <p className="text-lg text-white capitalize font-bold my-8 leading-[32px] md:w-2/3">Lustrous yet understated. The new evening wear collection exclusively offered at the reopened Giorgio Amani boutique in Los Angeles</p>
+                    <button className="px-6 py-2 bg-Black hover:bg-black text-white rounded-sm font-semibold">See Collections</button>
                 </div>
+                <div className="md:w-1/2"></div>
             </div>
         </div>
     )
